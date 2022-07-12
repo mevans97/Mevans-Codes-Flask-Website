8447061ad7be29f52437c1ff41216cfd28b26847
Michael Evans <88016843+mevans97@users.noreply.github.com>
2022-07-12 13:13:51 -0400

    3 HTML pages and Nav Bar added
    Created 3 HTML pages for the three use cases of my site and updated the nav bar to connect the three on each page of the site.

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
         return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+        return render_template("projects.html")
+
+@app.route("/tutorials")
+def tutorials():
+        return render_template("tutorials.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
